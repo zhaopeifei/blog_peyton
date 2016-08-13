fe287a0209f1cb15d20e0e62e2291a08d453eb2f
zhaopeifei <949272881@qq.com>
2016-08-13 22:17:24 +0800

    添加留言板页面

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/', function(req, res, next){
     res.render('comments', { sources: [
-        { source: "/bower_components/bootstrap/dist/css/bootstrap.css" }
+        { source: "/bower_components/bootstrap/dist/css/bootstrap.css" },
+        { source: "/css/comments.css"}
     ]});
 });
 
